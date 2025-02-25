50ee0dfedcbc79152a663fe344aaa7434ae3d156
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-25 19:44:55 +0330

    add item_recyclerview.xml

---
+++ app/src/main/java/ir/hajkarami/groceryapp/MyAdapter.kt
@@ -0,0 +1,29 @@
+package ir.hajkarami.groceryapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_recyclerview, parent, false)
+
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
